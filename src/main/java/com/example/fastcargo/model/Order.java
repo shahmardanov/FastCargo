f2d953de7c6a0f8a3cf03e135236fe44fc53db7f
shamil <shahmardanov.shamil@gmailcom>
2024-10-20 13:31:31 +0400

    add order class

--- src/main/java/com/example/fastcargo/model/Order.java
+++ src/main/java/com/example/fastcargo/model/Order.java
@@ -1,4 +1,28 @@
 package com.example.fastcargo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderName;
+    private String destination;
+    private String status;
+    private String courierName;
+    private String userName;
+
 }
