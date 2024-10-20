189d849cba852ae310d12ba541f809c1046a532d
shamil <shahmardanov.shamil@gmailcom>
2024-10-20 13:11:53 +0400

    add all dependencies

---
+++ src/main/java/com/example/fastcargo/FastCargoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastcargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCargoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCargoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastcargo/FastCargoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastcargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCargoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
