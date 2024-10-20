0cac69a393e816eaa136508e5e83cc0da390cea5
shamil <shahmardanov.shamil@gmailcom>
2024-10-20 13:17:12 +0400

    created user class

---
+++ src/main/java/com/example/fastcargo/model/User.java
@@ -0,0 +1,26 @@
+package com.example.fastcargo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String userSurname;
+
+    @Column(unique = true)
+    private String userEmail;
+    private String password;
+}
